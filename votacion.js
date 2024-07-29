6a7d82021fc4f0594d1b9d9afc90e7790a212974
Aniba1Gatica <anibal.gatica1701@gmail.com>
2024-07-29 18:10:23 -0400

    introduccion al sistema de votacion

---
+++ votacion.js
@@ -0,0 +1,48 @@
+$(document).ready(function() {
+    $("#formulario-votación").on("submit", function(event) {
+      event.preventDefault();
+  
+      var votante = $("#votante").val();
+      var alias = $("#alias").val();
+      var rut = $("#rut").val();
+      var correo = $("#correo").val();
+      var region = $("#id_region").val();
+      var comuna = $("#id_comuna").val();
+      var candidato = $("#id_candidato").val();
+      var webChecked = $("#web").is(":checked")?1:0;
+      var TVChecked = $("#TV").is(":checked")?1:0;
+      var redesSocialesChecked = $("#redesSociales").is(":checked")?1:0;
+      var amigoChecked = $("#amigo").is(":checked")?1:0;
+      
+      var datos = {
+        votante: votante,
+        alias: alias,
+        rut: rut,
+        correo: correo,
+        region: region,
+        comuna: comuna,
+        candidato: candidato,
+        web: webChecked,
+        TV: TVChecked,
+        redesSociales: redesSocialesChecked,
+        amigo: amigoChecked
+      };
+  
+      if (webChecked || TVChecked || redesSocialesChecked || amigoChecked) {
+        $.ajax({
+          type: "POST",
+          url: "conexiones.php",
+          data: datos,
+          success: function(response) {
+            if (response === "success") {
+              $("#mensaje").html("Voto registrado correctamente");
+            } else {
+              $("#mensaje").html("Error al registrar el voto: " + response);
+            }
+          }
+        });
+      } else {
+        $("#mensaje").html("Por favor seleccione al menos una opción para cómo se enteró de nosotros.");
+      }
+    });
+  });
